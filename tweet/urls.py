1a291344c95a1cb94a88d651f69138f557005bbf
RyanCccc <chenrd769@gmail.com>
2014-04-29 20:19:16 -0400

    Finished tweet functions

--- tweet/urls.py
+++ tweet/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('tweet.views',
     url(r'^load_more_tweets/$', 'load_more_tweets', name='tweet_load_more_tweets'),
+    url(r'^reply/$', 'reply', name='tweet_reply'),
+    url(r'^post_tweet/$', 'post_tweet', name='tweet_post_tweet'),
+    url(r'^test_ajax/$', 'test_ajax', name='tweet_test_ajax')
 )
--- tweet/views.py
+++ tweet/views.py
@@ -6,29 +6,83 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from pee_user.models import PeeUser
-from tweet.models import Tweet
+from tweet.models import Tweet, Reply
 
 # Create your views here.
 @csrf_exempt
 def load_more_tweets(request):
     curr_pk = request.POST.get('curr_pk')
     user = request.user
-    my_user = PeeUser.objects.get(user=user)
-    last_tweet = Tweet.objects.get(pk=curr_pk)
-    last_time = last_tweet.timestamp
-    tweets = []
-    for tweet in Tweet.objects.filter(timestamp__lt=last_time).order_by('-timestamp'):
-        if tweet.author in my_user.followings:
-            tweets.append(tweet)
-            if len(tweets)>=10:
-                break
-    context = []
+    try:
+        my_user = PeeUser.objects.get(user=user)
+    except:
+        return return_success(False)
+    followings = list(my_user.followings.all())
+    followings.append(my_user)
+    tweets = Tweet.objects.filter(author__in=followings, pk__gt=curr_pk).order_by('-timestamp')
+    if tweets:
+        curr_pk = tweets[0].pk
+    print 'Hello'
+    tweets_obj = []    
     for tweet in tweets:
-        tweet_json = {
-            'author': tweet.author.first_name + ' ' + tweet.author.last_name,
+        tweet_obj = {
+            'author': tweet.author.full_name(),
             'content': tweet.content,
-            'timestamp': str(tweet),
+            'timestamp': str(tweet.timestamp),
         }
-        context.append(tweet_json)
+        tweets_obj.append(tweet_obj)
+    notify_count = Tweet.objects.filter(author=my_user, has_new_reply=True).count()
+    context = {
+        'success':True,
+        'tweets':tweets_obj,
+        'curr_pk':curr_pk,
+        'notify_count':notify_count,
+    }
     context_j = json.dumps(context)
+    return HttpResponse(context_j, content_type="application/json")
+
+@csrf_exempt
+def reply(request):
+    user = request.user
+    try:
+        my_user = PeeUser.objects.get(user=user)
+    except:
+        return return_success(False)
+    author = my_user
+    param = request.POST
+    tweet_pk = param.get('tweet_pk')
+    reply_content = param.get('reply_content')
+    try:
+        reply = Reply.objects.new_reply(author, tweet_pk, reply_content)
+    except:
+        return return_success(False)   
+    if reply:
+        return return_success()
+    else:
+        return return_success(False)
+
+@csrf_exempt
+def post_tweet(request):
+    user = request.user
+    try:
+        my_user = PeeUser.objects.get(user=user)
+    except:
+        return return_success(False)
+    author = my_user
+    param = request.POST
+    content = param.get('content')
+    try:
+        tweet = Tweet.objects.create(author=author, content=content)
+    except:
+        return return_success(False)   
+    if tweet:
+        return return_success()
+    else:
+        return return_success(False)
+
+def test_ajax(request):
+    return render(request,'test_ajax.html')
+
+def return_success(success=True):
+    context_j = json.dumps({'success':success,})
     return HttpResponse(context_j, content_type="application/json")
